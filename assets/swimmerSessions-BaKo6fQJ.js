f6baf39f63fac3922fe0cc1303f9488181cd4dd1
erstein-aquatic-club <erstein-aquatic-club@users.noreply.github.com>
2026-06-10 17:43:00 +0000

    deploy: 255748be75d8d0848094a08ebccbdb989486f76c

--- assets/swimmerSessions-BaKo6fQJ.js
+++
@@ -1,1 +0,0 @@
-import{M as t,N as i,s as n}from"./index-Dd0juJ5V.js";async function u(s,a,e,r=!1){return t()?i(await n.rpc("get_swimmer_sessions",{p_user_id:s,p_from:a,p_to:e,p_include_drafts:r}))??[]:[]}export{u as g};
